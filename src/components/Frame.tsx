2dd8032c3151cb441b8405a5d40bfdb0daf54d5e
dats1ck27 <debjyoti.dasghosh@live.com>
2023-04-03 14:11:52 +0530

    Added features
    Humidity, Max Speed, some styling and isLoading for Next 7 Days Forecast button

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
 import axios from "axios";
+import { useState } from "react";
 import Time from "./Time";
 
 export default function Frame() {
@@ -9,6 +9,8 @@ export default function Frame() {
   const [icon, setIcon] = useState<string>("");
   const [cond, setCond] = useState<string>();
   const [next, setNext] = useState<any[]>([]);
+  const [isloading, setIsLoading] = useState<boolean>(false);
+
   const showResponse = async (location: string, forecast: string) => {
     setLoc(location);
     setTemp(forecast);
@@ -40,12 +42,18 @@ export default function Frame() {
   function fetchForecastData(lat: number, long: number) {
     axios
       .get(
-        `https://api.weatherapi.com/v1/forecast.json?key=eea7fd6ff63e4602977155650230104&q=${lat},${long}&days=7`
+        `http://api.weatherapi.com/v1/forecast.json?key=eea7fd6ff63e4602977155650230104&q=${lat},${long}&days=7`
       )
       .then((response) => {
         let week_data: Array<any> = response.data.forecast["forecastday"].map(
           (item: any) => {
-            return { date: item.date, avgtemp: item.day.avgtemp_c , icon:item.day.condition.icon};
+            return {
+              date : item.date,
+              avgtemp: item.day.avgtemp_c,
+              icon: item.day.condition.icon,
+              humidity: item.day.avghumidity,
+              maxwind: item.day.maxwind_kph,
+            };
           }
         );
         setNext(week_data);
@@ -54,12 +62,20 @@ export default function Frame() {
       .catch();
   }
   const forecastHandler = () => {
+    setIsLoading(true);
     navigator.geolocation.getCurrentPosition((position) => {
       const lat: number = position.coords.latitude;
       const long: number = position.coords.longitude;
       fetchForecastData(lat, long);
     });
   };
+
+  const isLoadingHandler = () =>{
+    setIsLoading(false);
+    setIcon("");
+    setCond("");
+    setNext([]);
+  }
   let message = (
     <div className="grid justify-items-center">
       <Time />
@@ -68,17 +84,40 @@ export default function Frame() {
         Hey, its {cond} out there. Your Location is {loc} and Temperature is{" "}
         {temp}
       </p>
-      <button
+      {!isloading && <button
         className="rounded-full bg-sky-500/75 hover:bg-sky-500/100 px-5 py-3 my-5"
         onClick={forecastHandler}
       >
         See Next 7 Days Forecast
-      </button>
+      </button> }
+      {isloading && <button
+        className="rounded-full bg-sky-500/75 hover:bg-sky-500/100 px-5 py-3 my-5"
+        onClick={isLoadingHandler}
+      >
+        Close The Forecast
+      </button> }
       <div>
         {next.map((item) => (
-          <div className="flex rounded-lg border-solid border-2 border-sky-500 my-5">
+          <div className="flex rounded-lg border-solid border-2 shadow-md border-sky-500 my-5">
             <img src={item.icon} alt="" />
-            <p className="font-mono text-base my-5 mb-5 p-4 w-full md:w-50% ">Day:{item["date"]} Temperature:{item["avgtemp"]}</p>
+            <div className="flex-auto">
+              <p className="font-mono text-base my-6 mb-4 md:w-50% text-3xl">
+                {item["avgtemp"]}°C
+              </p>
+            </div>
+            <div className="flex p-1">
+              <p className="font-mono text-base my-7 mx-2">
+                {item["date"]}
+              </p>
+              <div className="p-4">
+              <p className="font-mono text-base mx-14">
+                Humidity:{item["humidity"]}%
+              </p>
+              <p className="font-mono text-base ">
+              Maximum Wind Speed: {item["maxwind"]} kmph
+              </p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
