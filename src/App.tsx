771dac1fbb55195503c024feabbd9cb26aabb14c
dats1ck27 <debjyoti.dasghosh@live.com>
2023-04-03 21:59:48 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "./App.css";
 import Frame from "./components/Frame";
+import Time from "./components/Time";
+import Search from "./components/Search";
+import Footer from "./components/Footer";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
   return (
-    <div className="container mx-auto grid justify-items-center box-border h-50 w-100 bg-" >
+    <div className="container mx-auto grid justify-items-center box-border h-50 w-100 bg-">
+      <Search />
+      <Time />
       <Frame />
-    </div>
-    
       
+
+      <Footer />
+    </div>
   );
 }
 
